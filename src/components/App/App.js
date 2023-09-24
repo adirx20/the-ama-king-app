730f07ef4c5bc635617d890c3d7b673ea5c83612
adirx20 <87542990+adirx20@users.noreply.github.com>
2023-09-24 04:56:30 +0300

    edit App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Betting from './Betting';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import firebase from 'firebase/app';
+import firebase from 'firebase/auth';
+import firebaseConfig from '../../firebaseConfig';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='header'>
+          <h2>
+            Sports Betting App
+          </h2>
+          {/* Add navigation links or components here */}
+        </header>
+        <main>
+          <Switch>
+            {/* Define routes for your app */}
+            <Route path='/' exact component={Betting} />
+            {/* Add more routes as needed */}
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
