7447700c67b56d5cda60299bdc95d1fbeaf58cb7
adirx20 <87542990+adirx20@users.noreply.github.com>
2023-09-24 05:58:58 +0300

    implement betting logic

--- src/components/Betting/Betting.js
+++ src/components/Betting/Betting.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 function Betting() {
     const [upcomingGames, setUpcomingGames] = useState([]);
+    const [selectedGame, setSelectedGame] = useState(null);
+    const [homeTeamScore, setHomeTeamScore] = useState('');
+    const [awayTeamScore, setAwayTeamScore] = useState('');
+
     const apiUrl = 'API_URL';
 
     useEffect(() => {
@@ -18,23 +22,72 @@ function Betting() {
             });
     }, [apiUrl]);
 
+    // Function to handle when the user selects a game to predict
+    const handleGameSelect = (game) => {
+        setSelectedGame(game);
+    };
+
+    // Function to handle when the user predicts scores
+    const handlePredictScores = () => {
+        // Ensure the user has selected a game and entered valid scores
+        if (
+            selectedGame
+            && homeTeamScore.trim() !== ''
+            && awayTeamScore.trim() !== ''
+        ) {
+            // TODO: Implement logic to record user predictions
+            // You can store the predictions in your frontend state or context API for now
+            const userPredictions = {
+                gameId: selectedGame.id,
+                homeTeamScore: parseInt(homeTeamScore),
+                awayTeamScore: parseInt(awayTeamScore),
+            };
+
+            // You may want to update the state with the user's prediction
+            // Example: updateUserPredictions(userPrediction);
+
+            // Clear the selected game and predicted scores
+            setSelectedGame(null);
+            setHomeTeamScore('');
+            setAwayTeamScore('');
+        } else {
+            // Display an error message to the user if the input is invalid
+            alert('Please select a game and predict scores for both teams.');
+        }
+    };
+
     return (
         <div>
             <h2>Upcoming Games</h2>
             <ul>
                 {upcomingGames.map((game) => (
                     <li key={game.id}>
-                        {/* Display game details, e.g., teams, match date */}
-                        <p>
-                            Teams: {game.teams}
-                        </p>
-                        <p>
-                            Match Date: {game.matchDate}
-                        </p>
-                        {/* Add more details as needed later */}
+                        <p>Teams: {game.teams}</p>
+                        <p>Match Date: {game.matchDate}</p>
+                        <button onClick={() => handleGameSelect(game)}>Predict Scores</button>
                     </li>
                 ))}
             </ul>
+
+            {selectedGame && (
+                <div>
+                    <h3>Predict Scores</h3>
+                    <p>Selected Game: {selectedGame.teams}</p>
+                    <input
+                        type="number"
+                        placeholder="Home Team Score"
+                        value={homeTeamScore}
+                        onChange={(e) => setHomeTeamScore(e.target.value)}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Away Team Score"
+                        value={awayTeamScore}
+                        onChange={(e) => setAwayTeamScore(e.target.value)}
+                    />
+                    <button onClick={handlePredictScores}>Predict</button>
+                </div>
+            )}
         </div>
     );
 }
